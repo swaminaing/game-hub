902baf34cabed976ffecb65387700d7b8b22bf4f
Swam-Min-Naing <smnaing.smn@gmail.com>
2024-11-22 21:37:13 +0630

    Make Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
-function App(){
-  return(
-    <Button colorScheme='blue'>Click</Button>
-  )
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"gold"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
