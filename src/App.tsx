ad307415c3350fbf0cc3ad5f3ce51bd562b4559d
Swam-Min-Naing <smnaing.smn@gmail.com>
2024-11-22 21:58:24 +0630

    build a navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area={"nav"} bg={"coral"}>
-        Nav
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} bg={"gold"}>
