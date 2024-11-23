c0b2271eb51bda19d3027beeca87b44ded30f0e7
Swam-Min-Naing <smnaing.smn@gmail.com>
2024-11-23 22:43:16 +0630

    remove duplicate style

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface GameCardProps {
 
 function GameCard({ game }: GameCardProps) {
   return (
-    <Card width='300px' borderRadius={10} overflow="hidden">
+    <Card width='300px'>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,19 +2,26 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
-
+import GameCardContainer from "./GameCardContainer";
 
 function GameGrid() {
-  const {games, error, isLoading} = useGame();
-  const skeletons = [1, 2, 3, 4, 5, 6]
+  const { games, error, isLoading } = useGame();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <div>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10}>
-        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </div>
