962f8e15bf9b67e85c65df5f26fdc4dcf3f85afe
Swam-Min-Naing <smnaing.smn@gmail.com>
2024-11-23 20:44:05 +0630

    loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,18 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 
 function GameGrid() {
-  const {games, error} = useGame();
+  const {games, error, isLoading} = useGame();
+  const skeletons = [1, 2, 3, 4, 5, 6]
 
   return (
     <div>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10}>
+        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
@@ -24,23 +24,30 @@ interface FetchGames {
 function useGame() {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
+
     apiClient
       .get<FetchGames>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
 
         setError(err.message);
+        setLoading(true);
       });
 
     return () => controller.abort();
   }, []); // Add an empty array here to run the effect only once
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGame;
