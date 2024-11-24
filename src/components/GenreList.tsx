981eb57cd9dee201109cd923208c0a469da07e19
swaminaing <smnaing.smn@gmail.com>
2024-11-24 21:25:45 +0630

    show error while fetching

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenre();
+  const { data, isLoading, error } = useGenre();
+
+  if(error) return null
+  if(isLoading) return <Spinner />
 
   return (
     <List>
