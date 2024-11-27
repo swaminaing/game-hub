7bf9dac41d3407a8c1bfe1b1e298df46913b1309
swaminaing <smnaing9.smn@gmail.com>
2024-11-27 22:35:29 +0630

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGame";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface GameCardProps {
   game: Game;
@@ -21,7 +22,7 @@ function GameCard({ game }: GameCardProps) {
           />
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name} <Emoji rating={game.rating_top} /> </Heading>
       </CardBody>
     </Card>
   );
